ab830288ae50e3ea26e6d415990f586bb1795bc3
Luis Fernando Rivera Cortés <luisfernandoriveracortes@gmail.com>
2022-07-03 01:17:50 -0500

    add base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'http://react.rivecort.com/gh-react-vite/',
+  base: 'https://lfriveraco.github.io/gh-react-vite/',
   plugins: [react()]
 })
